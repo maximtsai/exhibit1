27b8a57c1a05838f6297c38234504d73f5821e61
maximtsai <maximtsai@yahoo.com>
2025-02-03 08:22:45 -0800

    poki sdk

--- sdkwrapper.js
+++ sdkwrapper.js
@@ -1,95 +1,84 @@
-let useSDK = !1;
+let useSDK = true;
 let crazysdk = null;
 
 function sdkWrapperInit() {
-    if (useSDK) {
-        crazysdk = window.CrazyGames.CrazySDK.getInstance();
-        crazysdk.init()
-    }
+	if (useSDK) {
+		PokiSDK.init().then(() => {
+		    console.log("Poki SDK successfully initialized");
+		    // fire your function to continue to game
+		}).catch(() => {
+		    console.log("Initialized, something went wrong, load you game anyway");
+		    // fire your function to continue to game
+		});
+	}
 }
 
 function sdkWrapperGameLoadingStart() {
-    if (useSDK) {
-        crazysdk.sdkGameLoadingStart()
-    }
+	if (useSDK) {
+		// crazysdk.sdkGameLoadingStart()
+	}
 }
 
 function sdkWrapperRequestResponsiveBanner(name) {
-    if (useSDK) {
-        crazysdk.requestResponsiveBanner([name])
-    }
+	if (useSDK) {
+		// crazysdk.requestResponsiveBanner([name])
+	}
 }
 
 function sdkWrapperGameLoadingStop() {
-    if (useSDK) {
-        crazysdk.sdkGameLoadingStop()
-    }
+	if (useSDK) {
+		PokiSDK.gameLoadingFinished();
+	}
 }
 
 function sdkWrapperGameplayStart() {
-    if (useSDK) {
-        crazysdk.gameplayStart()
-    }
+	if (useSDK) {
+		PokiSDK.gameplayStart();
+	}
 }
 
 function sdkWrapperGameplayStop() {
-    if (useSDK) {
-        crazysdk.gameplayStop()
-    }
+	if (useSDK) {
+		PokiSDK.gameplayStop();
+	}
 }
 
 function sdkWrapperClearAllBanners() {
-    if (useSDK) {
-        crazysdk.clearAllBanners()
-    }
+	if (useSDK) {
+
+	}
 }
 
-function sdkCommercialBreak(onStart, onFinish) {
-	if (useSDK) {
-	} else {
-                onFinish();
-    }
+function sdkCommercialBreak(onStart, onResume) {
+	PokiSDK.commercialBreak(() => {
+	// you can pause any background music or other audio here
+		onStart();
+	}).then(() => {
+	    // PokiSDK.gameplayStart();
+	    onResume();
+	  // if the audio was paused you can resume it here (keep in mind that the function above to pause it might not always get called)
+	  // continue your game here
+	});
+}
+
+
+function sdkRewardedBreak() {
+	PokiSDK.rewardedBreak(() => {
+	  // you can pause any background music or other audio here
+	}).then((success) => {
+	    if(success) {
+	        // video was displayed, give reward
+	    } else {
+	        // video not displayed, should not give reward
+	    }
+	    // if the audio was paused you can resume it here (keep in mind that the function above to pause it might not always get called)
+	    console.log("Rewarded break finished, proceeding to game");
+	    // continue your game here
+	});
 }
 
 function sdkWrapperResizeBanners() {
-    if (useSDK) {
-        var divTop = document.getElementById('banner-container-top');
-        var divBot = document.getElementById('banner-container-end');
-        let halfWidthStr = window.innerWidth * 0.5 + "px";
-        if (parseInt(game.canvas.style.height) < 400 || game.canvas.style.height == undefined) {
-            gameVars.showTinyBanner = !0
-        }
-        if (!gameVars.showingBannerBot) {
-            if (window.innerWidth < 970) {
-                divBot.style.width = window.innerWidth + "px";
-                divBot.style.left = "calc(50% - " + halfWidthStr + ")"
-            } else {
-                divBot.style.width = "970px";
-                divBot.style.left = "calc(50% - 485px)"
-            }
-            if (gameVars.showTinyBanner) {
-                divBot.style.height = "50px";
-                divBot.style.width = "320px";
-                divBot.style.left = "calc(50% - 160px)"
-            } else {
-                divBot.style.height = "120px"
-            }
-        }
-        if (!gameVars.showingBannerTop) {
-            if (window.innerWidth < 730) {
-                divTop.style.width = window.innerWidth + "px";
-                divTop.style.left = "calc(50% - " + halfWidthStr + ")"
-            } else {
-                divTop.style.width = "730px";
-                divTop.style.left = "calc(50% - 365px)"
-            }
-            if (gameVars.showTinyBanner) {
-                divTop.style.height = "50px";
-                divBot.style.width = "320px";
-                divBot.style.left = "calc(50% - 160px)"
-            } else {
-                divTop.style.height = "90px"
-            }
-        }
-    }
+	if (useSDK) {
+
+	}
 }
